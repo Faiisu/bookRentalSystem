ad8d543ab7dd642880e4ed3cef61b6f81e86b87d
ronaldodavinci <kavvin.cg@gmail.com>
2025-03-05 16:37:07 +0700

    Changing some backend directory + Books pages and api

--- backend/api_server/routes/product.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from server.database import db, serialize_product
-
-router = APIRouter()
-
-@router.get("/")
-async def get_products():
-    products = await db.products.find().to_list(100)  # Fetch products from MongoDB
-    return {"products": [serialize_product(product) for product in products]}  # ✅ Fix ObjectId
